c47a11702e34d0f4e7c231c0e5b54c983648c8d8
LudSkywalker <ludingnumpaque@gmail.com>
2021-07-28 01:35:20 -0500

    Creation of react proyect

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Graph } from './components/Graph';
+export const App = () => {
+	return (
+		<React.Fragment>
+			hola mundo
+			<Graph />
+		</React.Fragment>
+	);
+};
+ 
---
+++ client/src/components/Graph.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Graph = () => {
+    return (
+        <React.Fragment>
+            Grafica aqui
+        </React.Fragment>
+    );
+}
